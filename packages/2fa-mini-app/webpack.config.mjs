766e870c4c20f49efbf39cfd4f630141721ad729
Muhammad Azhar <muhammad.zahid@du.ae>
2025-05-19 00:51:01 +0400

    chore: Update Repack to 4

--- packages/2fa-mini-app/webpack.config.mjs
+++ packages/2fa-mini-app/webpack.config.mjs
@@ -4,21 +4,6 @@ import TerserPlugin from 'terser-webpack-plugin';
 import * as Repack from '@callstack/repack';
 
 const STANDALONE = Boolean(process.env.STANDALONE);
-/**
- * More documentation, installation, usage, motivation and differences with Metro is available at:
- * https://github.com/callstack/repack/blob/main/README.md
- *
- * The API documentation for the functions and plugins used in this file is available at:
- * https://re-pack.netlify.app/
- */
-
-/**
- * Webpack configuration.
- * You can also export a static object or a function returning a Promise.
- *
- * @param env Environment options passed from either Webpack CLI or React Native CLI
- *            when running with `react-native start/bundle`.
- */
 export default env => {
   const {
     mode = 'development',
@@ -39,42 +24,14 @@ export default env => {
     throw new Error('Missing platform');
   }
 
-  /**
-   * Using Module Federation might require disabling hmr.
-   * Uncomment below to set `devServer.hmr` to `false`.
-   *
-   * Keep in mind that `devServer` object is not available
-   * when running `webpack-bundle` command. Be sure
-   * to check its value to avoid accessing undefined value,
-   * otherwise an error might occur.
-   */
-  // if (devServer) {
-  //   devServer.hmr = false;
-  // }
-
-  /**
-   * Depending on your Babel configuration you might want to keep it.
-   * If you don't use `env` in your Babel config, you can remove it.
-   *
-   * Keep in mind that if you remove it you should set `BABEL_ENV` or `NODE_ENV`
-   * to `development` or `production`. Otherwise your production code might be compiled with
-   * in development mode by Babel.
-   */
   process.env.BABEL_ENV = mode;
 
   return {
     mode,
-    /**
-     * This should be always `false`, since the Source Map configuration is done
-     * by `SourceMapDevToolPlugin`.
-     */
+ 
     devtool: false,
     context,
-    /**
-     * `getInitializationEntries` will return necessary entries with setup and initialization code.
-     * If you don't want to use Hot Module Replacement, set `hmr` option to `false`. By default,
-     * HMR will be enabled in development mode.
-     */
+ 
     entry: [
       ...Repack.getInitializationEntries(reactNativePath, {
         hmr: devServer && devServer.hmr,
@@ -82,30 +39,10 @@ export default env => {
       entry,
     ],
     resolve: {
-      /**
-       * `getResolveOptions` returns additional resolution configuration for React Native.
-       * If it's removed, you won't be able to use `<file>.<platform>.<ext>` (eg: `file.ios.js`)
-       * convention and some 3rd-party libraries that specify `react-native` field
-       * in their `package.json` might not work correctly.
-       */
+
       ...Repack.getResolveOptions(platform),
 
-      /**
-       * Uncomment this to ensure all `react-native*` imports will resolve to the same React Native
-       * dependency. You might need it when using workspaces/monorepos or unconventional project
-       * structure. For simple/typical project you won't need it.
-       */
-      // alias: {
-      //   'react-native': reactNativePath,
-      // },
     },
-    /**
-     * Configures output.
-     * It's recommended to leave it as it is unless you know what you're doing.
-     * By default Webpack will emit files into the directory specified under `path`. In order for the
-     * React Native app use them when bundling the `.ipa`/`.apk`, they need to be copied over with
-     * `Repack.OutputPlugin`, which is configured by default inside `Repack.RepackPlugin`.
-     */
     output: {
       clean: true,
       hashFunction: 'xxhash64',
@@ -114,9 +51,7 @@ export default env => {
       chunkFilename: '[name].chunk.bundle',
       publicPath: Repack.getPublicPath({platform, devServer}),
     },
-    /**
-     * Configures optimization of the built bundle.
-     */
+
     optimization: {
       /** Enables minification based on values passed from React Native CLI or from fallback. */
       minimize,
@@ -140,14 +75,6 @@ export default env => {
       chunkIds: 'named',
     },
     module: {
-      /**
-       * This rule will process all React Native related dependencies with Babel.
-       * If you have a 3rd-party dependency that you need to transpile, you can add it to the
-       * `include` list.
-       *
-       * You can also enable persistent caching with `cacheDirectory` - please refer to:
-       * https://github.com/babel/babel-loader#options
-       */
       rules: [
         {
           test: /\.[jt]sx?$/,
@@ -214,15 +141,6 @@ export default env => {
       ],
     },
     plugins: [
-      /**
-       * Configure other required and additional plugins to make the bundle
-       * work in React Native and provide good development experience with
-       * sensible defaults.
-       *
-       * `Repack.RepackPlugin` provides some degree of customization, but if you
-       * need more control, you can replace `Repack.RepackPlugin` with plugins
-       * from `Repack.plugins`.
-       */
       new webpack.DefinePlugin({
         isStandAlone: JSON.stringify(STANDALONE),
       }),
